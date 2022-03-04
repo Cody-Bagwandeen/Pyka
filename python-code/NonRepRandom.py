fc3bdcf3697ee8ca1af1115388fbf530e96cb1d3
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2022-03-03 22:13:15 -0500

    added comments and fixed logic error

--- python-code/NonRepRandom.py
+++ python-code/NonRepRandom.py
@@ -3,12 +3,12 @@ import random
 class NonRepRandom:
     def __init__(self, max):
         self.max = max
-        nums = list(range(0,max+1))
+        nums = list(range(0,max))
 
     def generateNum():
         if nums.len == 0 :
-            nums = list(range(0,max+1))
-        return nums.pop(random.randint(0,nums.len + 1))
+            nums = list(range(0,max+1))  # repopulates list if empyy
+        return nums.pop(random.randint(0,nums.len + 1)) # remove and return a random element from list
         
 
 
