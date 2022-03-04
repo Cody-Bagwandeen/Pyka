ebb342a38c429b70c703047e87bcf2e052fd16f3
Cody_Bagwandeen <codybagwandeen1@gmail.com>
2022-03-04 13:39:13 -0500

    debugged and now the game is functional

--- python-code/Game.py
+++ python-code/Game.py
@@ -6,23 +6,25 @@ class Game:
     def __init__(self, size=4):
         self.attemps = 0
         self.size = size
-        self.computernum = Pykanum(size)
+        self.computernum = Pykanum(None, size)
         self.gameFinished = False
 
     def isInvalidNum(self, val):
-        if len(val) != self.size :
+        if len(val) != self.size:
             return True
-        for i in range(self.size):
-            for j in range(self.size):
-                if i != j and self.computernum.num[i] == val[i]:
-                    return True
+        for x in val:
+            if val.count(x) > 1:
+                return True
+
         return False
 
     def askForGuess(self):
         guess = input('Enter a guess.\n')
         while self.isInvalidNum(guess):
-            guess = input('Enter a guess.\n')
+            guess = input('Invalid Input! Enter new a guess.\n')
         self.attemps += 1
-        result = self.computernum.CompareNums(guess)
+        g = Pykanum(guess)
+        result = self.computernum.CompareNums((g))
+        print(result)
         if result == '0 pyka and 4 centra.':
             self.gameFinished = True
--- python-code/Pykanum.py
+++ python-code/Pykanum.py
@@ -2,25 +2,24 @@ from NonRepRandom import NonRepRandom
 
 
 class Pykanum:
-    def __init__(self, size=4, pnum=None):
+    def __init__(self, pnum=None, size=4,):
+        self.num = []
         if pnum == None:  # genreate a new pykanum if there isn't one given
             self.size = size
-            self.num = []
             n = NonRepRandom()
             for i in range(4):
-                self.num.append(n.generateNum())
+                self.num.append(str(n.generateNum()))
         else:  # if a num is given, turn that num into a pyka num
             for x in pnum:
                 self.size = len(pnum)
-                self.num.insert(0, x)
+                self.num.append(x)
 
     def CompareNums(self, other):
         pyka = 0
         centra = 0
-        guess = Pykanum(other)
         for i in range(self.size):
             if self.num[i] == other.num[i]:
                 centra += 1
-            elif other.num.contains(self.num[i]):
+            elif other.num.count(self.num[i]) :
                 pyka += 1
         return f"{pyka} pyka and {centra} centra."
